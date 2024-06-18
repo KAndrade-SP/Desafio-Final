c421c9318eb72da4d17fdf8d6ebe329e21081870
KAndrade-SP <kaique.viana090@gmail.com>
2024-06-18 18:16:58 -0300

    style: style of contact page completed

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 
 import { FormContactData, contactSchema } from '../../types/ContactValidations'
+import BenefitsBar from '../../components/BenefitsBar'
 
 const Contact = () => {
 
@@ -34,24 +35,24 @@ const Contact = () => {
         </div>
       </section>
 
-      <section className="flex flex-col gap-10 py-10 mx-4 justify-center items-center">
+      <section className="flex flex-col gap-10 py-20 mx-4 justify-center items-center">
         <h2 className='font-semibold text-3xl'>Get In Touch With Us</h2>
-        <p className='text-caption text-center mt-4'>For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
+        <p className='text-caption text-center md:w-1/2'>For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
 
-        <div className="flex flex-row gap-10">
+        <div className="flex flex-col md:flex-row gap-10 md:gap-20">
 
           <div className="flex flex-col gap-10">
 
-            <div className="flex flex-row gap-2">
-              <img src="" alt="" />
+            <div className="flex flex-row gap-8">
+              <img src="https://final-challenge-compass.s3.us-east-2.amazonaws.com/icons/local.svg" alt="Local Icon" />
               <div className="flex flex-col gap-2">
                 <p className='font-semibold text-lg'>Address</p>
                 <p>236 5th SE Avenue, New York NY10000, United States</p>
               </div>
             </div>
 
-            <div className="flex flex-row gap-2">
-              <img src="" alt="" />
+            <div className="flex flex-row gap-8">
+              <img src="https://final-challenge-compass.s3.us-east-2.amazonaws.com/icons/phone.svg" alt="Phone Icon" />
               <div className="flex flex-col gap-2">
                 <p className='font-semibold text-lg'>Phone</p>
                 <p>Mobile: +(84) 546-6789</p>
@@ -59,8 +60,8 @@ const Contact = () => {
               </div>
             </div>
 
-            <div className="flex flex-row gap-2">
-              <img src="" alt="" />
+            <div className="flex flex-row gap-8">
+              <img src="https://final-challenge-compass.s3.us-east-2.amazonaws.com/icons/clock.svg" alt="Clock Icon" />
               <div className="flex flex-col gap-2">
                 <p className='font-semibold text-lg'>Working Time</p>
                 <p>Monday-Friday: 9:00 - 22:00</p>
@@ -71,14 +72,13 @@ const Contact = () => {
           </div>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className='font-semibold text-3xl text-black text-start mb-12'>Billing details</h2>
 
             <div className="mb-4">
               <label htmlFor="display-name" className="block text-sm font-medium text-black mb-4">Your name</label>
               <input
                 type="text"
                 id="display-name"
-                className="mt-1 block w-full px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
+                className="mt-1 block w-[300px] px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
                 {...register('name')}
               />
               {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>}
@@ -91,7 +91,7 @@ const Contact = () => {
               <input
                 type="email"
                 id="email-address"
-                className="mt-1 block w-full px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
+                className="mt-1 block w-[300px] px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
                 {...register('email')}
               />
               {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
@@ -102,7 +102,7 @@ const Contact = () => {
               <input
                 type="text"
                 id="display-subject"
-                className="mt-1 block w-full px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
+                className="mt-1 block w-[300px] px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
                 {...register('subject')}
               />
               {errors.subject && <p className="text-red-500 text-sm mt-1">{errors.subject.message}</p>}
@@ -113,17 +113,20 @@ const Contact = () => {
               <label htmlFor="display-message" className="block text-sm font-medium text-black mb-4">Message</label>
               <textarea
                 id="display-message"
-                className="mt-1 block w-full px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
+                className="mt-1 block w-[300px] px-3 py-4 border border-carouselIndexGray rounded-lg shadow-sm focus:outline-none focus:ring-buttonDarkBrown focus:border-buttonDarkBrown sm:text-sm"
                 {...register('message')}
               />
               {errors.message && <p className="text-red-500 text-sm mt-1">{errors.message.message}</p>}
             </div>
 
-            <button type='submit' className='mt-4 px-20 py-3 self-center rounded-xl hover:bg-buttonBrown hover:border-buttonDarkBrown hover:text-white border border-titleGray'>Submit</button>
+            <button type='submit' className='mt-4 px-20 py-3 self-center rounded-md bg-buttonBrown text-white hover:bg-buttonDarkBrown focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-buttonDarkBrown'>Submit</button>
           </form>
 
         </div>
+
       </section>
+
+      <BenefitsBar />
     </>
   )
 }
