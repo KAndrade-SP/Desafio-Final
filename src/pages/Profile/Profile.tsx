d7ba5459bdf24a84649f00494695457bd5843ca5
KAndrade-SP <kaique.viana090@gmail.com>
2024-06-20 15:52:02 -0300

    feat: profile page completed

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -34,10 +34,17 @@ const Profile = () => {
     <>
       {user
         ?
-        <div>
-          {user?.photoURL && <img src={user?.photoURL} alt="UserProfilePhoto" className="rounded-full w-7 cursor-pointer hover:opacity-70"></img>}
-          <button onClick={handleLogout}>LOG OUT</button>
-        </div>
+        <section className="flex flex-col gap-10 items-center justify-center mx-auto py-20">
+          {user?.photoURL && <img src={user?.photoURL} alt="UserProfilePhoto" className="rounded-full w-20 cursor-pointer hover:opacity-70"></img>}
+          <p className='font-medium text-xl text-titleGray text-center'>Name: <span className='text-caption'>{user.displayName}</span></p>
+          <p className='font-medium text-xl text-titleGray text-center'>E-mail: <span className='text-caption'>{user.email}</span></p>
+          <div className="flex flex-col md:flex-row gap-8">
+            <button onClick={handleLogout} className='px-20 py-3 self-center rounded-xl text-buttonBrown font-bold bg-white hover:bg-buttonDarkBrown border-2 border-buttonBrown hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-buttonDarkBrown'>LOG OUT</button>
+            <Link to='/'>
+              <button className='px-20 py-3 self-center rounded-xl text-buttonBrown font-bold bg-white hover:bg-buttonDarkBrown border-2 border-buttonBrown hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-buttonDarkBrown'>Go back</button>
+            </Link>
+          </div>
+        </section>
         :
         <section className="flex flex-col gap-10 items-center justify-center mx-auto py-20">
           <h3 className="font-medium text-3xl text-titleGray text-center">You don't have a profile, please sign in to continue</h3>
@@ -47,7 +54,6 @@ const Profile = () => {
         </section>
       }
     </>
-
   )
 }
 
